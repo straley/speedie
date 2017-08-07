12f867e4aae5769276cb8d08f75ef578c61bfd5a
Scott Straley <sstraley@truecar.com>
2017-08-07 00:06:36 -0500

    working on math

--- speedie.py
+++ speedie.py
@@ -4,11 +4,33 @@ import yaml
 from movement.servos import Servos
 
 servos = Servos(yaml.load(file('servos.yaml', 'r')))
+# servos.center()
 
-print "test"
-servos.move('rear-left', 'hip', 90);
-servos.move('rear-right', 'hip', 90);
+step = {
+    'front-right': {
+        'x': 1,
+        'y': 1,
+        'z': 1,
+    },
+    'front-left': {
+        'x': 0,
+        'y': 0,
+        'z': 0,
+    },
+    'rear-right': {
+        'x': 0,
+        'y': 0,
+        'z': 0,
+    },
+    'rear-left': {
+        'x': 1,
+        'y': 1,
+        'z': 1,
+    },
+}
 
-time.sleep( 1.5 )
-print "center"
-servos.center()
+x = servos.body_ik( step, -0.1 );
+print x
+
+#servos.move('rear-left', 'hip', 90);
+#servos.move('rear-right', 'hip', 90);
